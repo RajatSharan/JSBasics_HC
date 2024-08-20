89b4c13d666227335593409233c88a3d6a975b32
Rajat Sharan <errajatsharan@gmail.com>
2024-08-20 18:47:57 +0000

    Variable

---
+++ 01_BasicJS/01_variable.js
@@ -0,0 +1,14 @@
+const accountId=1443
+let accountEmail="rajatsharan05@gmail.com"
+var accountpassword="12345"
+accountcity="Jaipur"
+
+accountID=2 //not allowed
+
+accountEmail="rs@rs.com"
+accountpassword="125434"
+accountcity="Delhi"
+let accountState;
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountpassword,accountcity,accountState])
