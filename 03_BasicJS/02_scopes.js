4d58202af04d752f7e6774502c630fc9ad0e63aa
Rajat Sharan <errajatsharan@gmail.com>
2024-08-27 01:03:28 +0530

    scopes

--- 03_BasicJS/02_scopes.js
+++ 03_BasicJS/02_scopes.js
@@ -8,6 +8,58 @@ if(true){
 
 
 
-console.log(a)
+//console.log(a)
 //console.log(b)
 //console.log(c)
+
+function one(){
+
+const userName="Rajat"
+function two(){
+
+    const website ="LinkedIN"
+    console.log(userName)
+}
+
+//console.log(website)
+two()
+}
+//one()
+
+if(true){
+
+    const userName="Rajat"
+    if(userName==="Rajat"){
+
+        const website="youtube"
+      //  console.log(userName+website);
+        
+    }
+
+    //console.log(website);
+    
+
+}
+
+//console.log(userName);
+
+
+//++++++++++++++++++++Interesting+++++++++++++++++/
+
+console.log(addone(5));
+ 
+
+function addone(num){
+
+    return num +1 
+}
+
+
+
+
+const addTwo= function(num){
+
+    return num+2
+}
+
+console.log(addTwo(5));
